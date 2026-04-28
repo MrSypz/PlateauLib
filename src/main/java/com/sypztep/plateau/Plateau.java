4f8dc21cd0bef8f417b5fea588de8c9ad5eab943
chain <chainot@hotmail.co.th>
2026-04-28 10:31:36 +0700

    remove unneed data

--- src/main/java/com/sypztep/plateau/Plateau.java
+++ src/main/java/com/sypztep/plateau/Plateau.java
@@ -1,12 +1,8 @@
 package com.sypztep.plateau;
 
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Plateau implements ModInitializer {
-    public static final String MODID = "assets/plateau";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
     @Override
     public void onInitialize() {
     }
